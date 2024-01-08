b7e648e677dae990534e88f44202bfe41009a7e8
AlexLatt <aleksandlatypov@gmail.com>
2024-01-09 02:59:09 +0300

    create ormconfig and tag.entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Module } from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { AppController } from '@app/app.controller';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { AppService } from '@app/app.service';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { TagModule } from '@app/tag/tag.module';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { TypeOrmModule } from '@nestjs/typeorm';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import ormconfig from '@app/ormconfig';
 
 @Module({
-  imports: [TagModule],
+  imports: [TypeOrmModule.forRoot(ormconfig), TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export class AppModule {}
